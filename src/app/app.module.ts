a355e863babdb20d7c9f36051fdd1f9fcbebb1a5
Harshil Parlikar <harshil.parlikar@valuemomentum.com>
2022-08-05 11:47:37 +0530

    bookapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { ListBookComponent } from './list-book/list-book.component';
+import { ViewBookComponent } from './view-book/view-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { ListOrdersComponent } from './list-orders/list-orders.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    ListBookComponent,
+    ViewBookComponent,
+    EditBookComponent,
+    
+    HeaderComponent,
+    ListOrdersComponent,
+    LoginComponent,
+    MyOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
